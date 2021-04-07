24e4f629feff9ba31001ddbe649faafa1fb4bc29
cristians775 <cristiansaldivia775@gmail.com>
2021-04-07 00:17:29 -0300

    añadiendo metodos

--- server/src/Core/Repositories/User.repository.ts
+++ server/src/Core/Repositories/User.repository.ts
@@ -1,15 +1,30 @@
-import { IUser } from "../../Interfaces/User/User.interface";
 import { IUserRepository } from "../../Interfaces/Repositories/User.interface";
 import { IGetUser } from "../../Interfaces/User/GetUser.interface";
-import UserModel from "../Entities/Models/User.model";
+import UserModel from "../Models/User.model";
+import { IUserDocument } from "../../Interfaces/User/UserDocument.interface";
+import { IAddUserInput } from "../../Interfaces/User/AddUserInput.interface";
 
 export class UserRepository implements IUserRepository {
   public findAll = async (): Promise<IGetUser[]> => {
     const user: IGetUser[] = await UserModel.find();
     return user;
   };
-  public findOneByEmail = async (email: string): Promise<IGetUser> => {
-    const user: IGetUser = await UserModel.findOne({ email });
+  public findById = async (id: string | string[]): Promise<IGetUser> => {
+    const user: IGetUser | IGetUser[] = await UserModel.findById(id);
     return user;
+  };
+
+  public async exists(username: { username: string }): Promise<boolean>;
+  public async exists(email: { email: string }): Promise<boolean>;
+  public async exists(data: any) {
+    const { username, email } = data;
+    return username
+      ? await UserModel.exists({ username })
+      : await UserModel.exists({ email });
+  }
+
+  public add = async (user: IAddUserInput): Promise<IGetUser> => {
+    const newUser: IUserDocument = new UserModel(user);
+    return await newUser.save();
   };
 }
