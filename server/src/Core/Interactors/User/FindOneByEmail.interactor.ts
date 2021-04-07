225700599162eda3c2effe0b3b7d3dbdc803a6f2
cristians775 <cristiansaldivia775@gmail.com>
2021-04-07 00:13:04 -0300

    borrando archivos

--- server/src/Core/Interactors/User/FindOneByEmail.interactor.ts
+++
@@ -1,8 +0,0 @@
-import { UserRepository } from "../../Repositories/User.repository";
-
-
-export const findOne =(userRepository:UserRepository)=> async (email:string)=>{
-    const UserRepository: UserRepository = userRepository;
-    const users:any = UserRepository.findOneByEmail(email);
-    return users;
-}
