e4bf53c4de4c51680b2da3625bc93451541a8170
cristians775 <cristiansaldivia775@gmail.com>
2021-04-07 00:16:17 -0300

    añadiendo interactors

--- server/src/Core/Interactors/User/index.ts
+++ server/src/Core/Interactors/User/index.ts
@@ -1,7 +1,9 @@
 import { UserRepository } from "../../Repositories/User.repository";
+import { add } from "./AddUser.interactor";
 import { findAll } from "./FindAll.interactor";
-import { findOne } from "./FindOneByEmail.interactor";
+import { findById } from "./FindById.interactor";
 const userRepository = new UserRepository();
 
 export const findAllUsers = findAll(userRepository); 
-export const findUserByEmail = findOne(userRepository);
+export const findUserById = findById(userRepository);
+export const addUser = add(userRepository);
