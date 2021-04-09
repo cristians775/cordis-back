1c6cf12fd27f6f1c3ba644fa4c3101082d954eef
cristians775 <cristiansaldivia775@gmail.com>
2021-04-09 19:22:59 -0300

    modificando interfaces

--- server/src/Interfaces/Repositories/User.interface.ts
+++ server/src/Interfaces/Repositories/User.interface.ts
@@ -1,7 +1,8 @@
+import { IGetUser } from "../User/GetUser.interface";
 import { IUser } from "../User/User.interface";
-export interface IUserRepository{
-    findAll():Promise<IUser[]>;
-    findById(id:string):Promise<IUser>;
-    exists(email:{email:string}):Promise<boolean>;
-    exists(username:{username:string}):Promise<boolean>;
+export interface IUserRepository {
+  findAll(): Promise<IGetUser[]>;
+  findById(id: string | number[]): Promise<IGetUser | IGetUser[]>;
+  exists(email: { email: string }): Promise<boolean>;
+  exists(username: { username: string }): Promise<boolean>;
 }
--- server/src/Interfaces/User/GetUser.interface.ts
+++ server/src/Interfaces/User/GetUser.interface.ts
@@ -4,6 +4,7 @@ export interface IGetUser {
   lastname: IUser["lastname"];
   username: IUser["username"];
   email: IUser["email"];
+  password?:IUser["password"];
   avatar?: IUser["avatar"];
   description: IUser["description"];
   createdAt?: IUser["createdAt"];
