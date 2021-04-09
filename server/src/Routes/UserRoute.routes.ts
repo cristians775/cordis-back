4df0d36118e8e0f25c8e86ebc176b38e7cf8b8e6
cristians775 <cristiansaldivia775@gmail.com>
2021-04-09 19:24:16 -0300

    eliminando param

--- server/src/Routes/UserRoute.routes.ts
+++ server/src/Routes/UserRoute.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import UserCtrl from '../Controllers/User.controller';
 const UserRoute: Router = Router();
 
-UserRoute.get("/:id",UserCtrl.getUserById);
+UserRoute.get("/",UserCtrl.getUserById);
 UserRoute.post("/",UserCtrl.addUser);
 
 export default UserRoute;
