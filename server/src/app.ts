dbcd94d303f567fbf257caf3906994cdb71024f1
cristians775 <cristiansaldivia775@gmail.com>
2021-04-10 20:21:22 -0300

    identando

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,27 +3,23 @@ import DataBase from "./Database/Database";
 import cors from "cors";
 import morgan from "morgan";
 import { routes } from "./Routes/routes";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
+dotenv.config({ path: ".env" });
 
-dotenv.config({path:".env"});
-
-const app:Application = express();
-const port:string =  process.env.SERVER_PORT;
+const app: Application = express();
+const port: string = process.env.SERVER_PORT;
 
 //DB CONNECTION
 DataBase.connect();
-
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
 // ROUTES
-routes.forEach((route:any) =>app.use(route.path, route.route));
-
+routes.forEach((route: any) => app.use(route.path, route.route));
 
 app.listen(port, () => {
   console.log(`Server running in port: ${port}`);
 });
-
