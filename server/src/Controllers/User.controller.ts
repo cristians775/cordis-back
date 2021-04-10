2e75c2d6f602ec0d276f3101737fc1d77ca971bb
cristians775 <cristiansaldivia775@gmail.com>
2021-04-10 20:19:48 -0300

    modificando controller

--- server/src/Controllers/User.controller.ts
+++ server/src/Controllers/User.controller.ts
@@ -1,30 +1,42 @@
-import { findUserById } from "../Core/Interactors/User/index";
-import { addUser } from "../Core/Interactors/User/index";
+import {
+  findUserById,
+  addUser,
+  removeUser,
+} from "../Core/Interactors/User/index";
 import { Response, Request } from "express";
 import { IGetUser } from "../Interfaces/User/GetUser.interface";
 import { IAddUserInput } from "../Interfaces/User/AddUserInput.interface";
+
 class UserCtrl {
   public getUserById = async (req: Request, res: Response): Promise<void> => {
-    const { query } = req;
-    const { id }: any = query;
+    const { params, query } = req;
+    const { fields }: any = query;
+    const { id }: any = params;
     try {
-      const queryId = id.split(",");
-      const dataId: string | number[]= queryId.length > 1 ? queryId : queryId[0];
-      const data: IGetUser | IGetUser[] = await findUserById(dataId);
+      const data: IGetUser | IGetUser[] = await findUserById(id, fields);
       res.send(data);
     } catch (e) {
       console.log(e);
     }
   };
+
   public addUser = async (req: Request, res: Response): Promise<void> => {
     try {
       const { body } = req;
       const newUser: IAddUserInput = body;
       const data: IGetUser = await addUser(newUser);
       res.status(200).send(data);
-    } catch (e) {
-      res.send({ message: e.message });
-    }
+    } catch (e) {}
+  };
+
+  public removeUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { params } = req;
+      const { id }: any = params;
+      const paramId = id.split(",");
+      const data: any = await removeUser(paramId);
+      res.send(data);
+    } catch (e) {}
   };
 }
 
