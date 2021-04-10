ce14524102c27603b84dbfbbd815fe2cb7af2610
cristians775 <cristiansaldivia775@gmail.com>
2021-04-10 20:20:00 -0300

    identando

--- server/src/Core/Models/User.model.ts
+++ server/src/Core/Models/User.model.ts
@@ -1,45 +1,46 @@
-import mongoose,{ Model, model, Types, Schema, Query } from "mongoose";
+import mongoose, { Model, model, Types, Schema, Query } from "mongoose";
 import { IUserModel } from "../../Interfaces/User/UserModel.interface";
 import { IUserDocument } from "../../Interfaces/User/UserDocument.interface";
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     name: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     username: {
-        type: String,
-        require: true,
-        trim: true,
-        unique: true,
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
     },
     email: {
-        type: String,
-        require: true,
-        trim: true,
-        unique: true
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
     },
     avatar: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
     description: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
     password: {
-        type: String,
-        require: true,
-        trim: true
+      type: String,
+      require: true,
+      trim: true,
     },
     createdAt: {
-        type: Date,
-        default: Date.now()
-    }
-},{
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-    versionKey: false
-
-});
-
-export default mongoose.model<IUserDocument ,IUserModel>("users",UserSchema)
+export default mongoose.model<IUserDocument, IUserModel>("users", UserSchema);
