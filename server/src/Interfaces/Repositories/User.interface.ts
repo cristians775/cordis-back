12dbcdd08d8d1ff32015155139dca00383ff07c9
cristians775 <cristiansaldivia775@gmail.com>
2021-04-10 20:21:13 -0300

    identando

--- server/src/Interfaces/Repositories/User.interface.ts
+++ server/src/Interfaces/Repositories/User.interface.ts
@@ -1,8 +1,12 @@
 import { IGetUser } from "../User/GetUser.interface";
-import { IUser } from "../User/User.interface";
+
 export interface IUserRepository {
   findAll(): Promise<IGetUser[]>;
-  findById(id: string | number[]): Promise<IGetUser | IGetUser[]>;
+  findById(
+    id: string | string[],
+    fields: string
+  ): Promise<IGetUser | IGetUser[]>;
   exists(email: { email: string }): Promise<boolean>;
   exists(username: { username: string }): Promise<boolean>;
+  remove(id: string | string): Promise<any>;
 }
